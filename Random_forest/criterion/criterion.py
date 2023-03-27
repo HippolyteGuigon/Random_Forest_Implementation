bb6474ae4bebb477d70bce314011a571a07a39ec
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-03-27 06:54:50 +0200

    pushing_variance_reduction_split

--- Random_forest/criterion/criterion.py
+++ Random_forest/criterion/criterion.py
@@ -53,5 +53,35 @@ def full_gini_compute(X: np.array, y: np.array)->tuple:
     split_pair = sorted(full_gini_impurities, key=lambda x: x[1])[0]
     return split_pair[0], split_pair[1]
 
+def variance_reduction(X: np.array, y: np.array)->tuple:
+    """
+    The goal of this function is to decide which value 
+    in a categorical column to choose for splitting 
+    by calculating the variance of each subset 
+    
+    Arguments:
+        -X: np.array: The column composed of multiple categorical 
+        variables 
+        -y: The target column 
+    
+    Returns:
+        -best_split: tuple: The best collumn with lowest variance 
+    """
+
+    variance_candidates = []
+    split_candidate=np.unique(X, return_counts=False)
+    full_data=np.hstack((X, y))
+
+    for candidate in split_candidate:
+        first_subset=full_data[full_data[:, 0]==candidate, -1].astype(float)
+        second_subset=full_data[full_data[:,0]!=candidate, -1].astype(float)
+        n=full_data.shape[0]
+        n_first_subset=first_subset.shape[0]
+        n_second_subset=second_subset.shape[0]
+        total_variance = (n_first_subset/n)*np.var(first_subset)+(n_second_subset/n)*np.var(second_subset)
+        variance_candidates.append((candidate, total_variance))
+    variance_candidates=sorted(variance_candidates,key=lambda x: x[1])
+    best_candidate=variance_candidates[0]
+    
 def entropy(x):
     pass
