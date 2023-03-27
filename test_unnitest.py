4527153b90436298b6746dcffd4cdc3ec3ee9e0d
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2023-03-27 03:06:04 +0200

    pushing_new_test

--- test_unnitest.py
+++ test_unnitest.py
@@ -24,10 +24,11 @@ class Test(unittest.TestCase):
         target = ["survivor", "non_survivor"]
         X=np.random.choice(random_categorical_values,size=(50, 5))
         y=np.random.choice(target, size=(50, 1))   
-        selected_column, _ =full_gini_compute(X, y)
+        selected_column, gini_score =full_gini_compute(X, y)
 
         self.assertEqual(gini_impurity(y, y), 0) 
         self.assertTrue(selected_column in range(X.shape[1]))
+        self.assertTrue(((gini_score<=1) and (gini_score>=0)))
 
 if __name__ == "__main__":
     unittest.main()
